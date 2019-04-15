367f6dfb80c31ea3c7f9eeef1faa4ef6b8d1c32b
adnansadar <adnansadar11@gmail.com>
2019-04-16 00:00:12 +0530

    adding program

--- practice/prac1.py
+++ practice/prac1.py
@@ -1,13 +1,2 @@
-l=input()
-maxcount=1
-count=1
-for i in range(len(l)-1):
-        for j in range(i+1,len(l)):
-                if l[i]!=l[j]:
-                        count+=1
-        if count>maxcount:
-                maxcount=count
-        count=1
-print(maxcount)
-        
-                
+t=(1,2,2,4,6)
+print(t.count(2))
